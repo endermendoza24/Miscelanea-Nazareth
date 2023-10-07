dc331c9f4e7c44a0d199c9c291c219c73e28b212
endermendoza24 <enderalonso24@gmail.com>
2023-10-07 17:08:21 -0600

    Agregados los catalogos

--- Domain.Endpoint/Entities/Producto.cs
+++ Domain.Endpoint/Entities/Producto.cs
@@ -10,6 +10,10 @@ namespace Domain.Endpoint.Entities
     {
         public int Id { get; set; }
         public string Descripcion_Producto { get; set; }
+        public DateTime Expiracion { get; set; }
+        public int Id_Categoria { get; set; }
+        public int Id_Marca { get; set; }
+        public int Id_UnidadMedida { get; set; }
 
     }
 }
---
+++ Domain.Endpoint/Entities/Proveedor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Endpoint.Entities
+{
+    public class Proveedor
+    {
+        public int Id { get; set; }
+        public string Nombre_Empresa { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set;}
+        public string CorreoElectronico { get; set; }
+    }
+}
